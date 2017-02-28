3aec807dcdc34261c28890c56adf41789c3ec76c
yitian <yi.t.xu@mail.mcgill.ca>
2017-02-27 19:24:33 -0500

    added background sub

--- robotics/project_17/src/sensor/src/camera/camera.py
+++ robotics/project_17/src/sensor/src/camera/camera.py
@@ -6,11 +6,22 @@
 # Image diff
 # http://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
 #
+# Background average
+# http://opencvpython.blogspot.ca/2012/07/background-extraction-using-running.html
+#
 # Background sub
-# http://docs.opencv.org/trunk/db/d5c/tutorial_py_bg_subtraction.html
+# https://www.hdm-stuttgart.de/~maucher/Python/ComputerVision/html/Tracking.html
+#
+# Min-cut/max-flow
+# http://pmneila.github.io/PyMaxflow/tutorial.html#complex-grids-with-add-grid-edges
+
+import numpy as np
+import scipy
+
+import maxflow
+import cv2
 
 import rospy
-import cv2
 from sensor_msgs.msg import Image
 # from std_msgs.msg import Header
 from cv_bridge import CvBridge, CvBridgeError
@@ -19,9 +30,15 @@ MIN_MOTION = 333
 OBS_BOX = 33
 SPIN_RATE = 0.33
 
+BACKGROUND_acc_weight = 0.01
+BACKGROUND_history = 10
+BACKGROUND_nGauss = 5
+BACKGROUND_bgThresh = 0.5
+BACKGROUND_noise = 10
+
 class camera:
 
-	def __init__(self, subTopic, method=1):
+	def __init__(self, subTopic, method=0):
 		self.image_sub = rospy.Subscriber(subTopic,Image,self.motion_callback)
 		self.bridge = CvBridge()
 		self.motion_pub = rospy.Publisher('motion_pub', Image, queue_size=10)
@@ -30,10 +47,14 @@ class camera:
 
 		if method == 1:
 			# background subtraction
-			self.avg1 = None
-			self.avg2 = None
+			self.background_avg = None
+			self.bgs = cv2.BackgroundSubtractorMOG(BACKGROUND_history,
+				BACKGROUND_nGauss,BACKGROUND_bgThresh,BACKGROUND_noise)
+		elif method == 2:
+			pass
 		else:
 			self.lastImage = None
+
 		self.method = method
 
 	def image_diff(self, image):
@@ -55,13 +76,13 @@ class camera:
 			(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 				cv2.CHAIN_APPROX_SIMPLE)
 
-			has_motion = False
+			# has_motion = False
 			# loop over the contours
-			for i, c in enumerate(cnts):
-				# if the contour is too small, ignore it
-				if cv2.contourArea(c) < MIN_MOTION:
-					continue
-				has_motion = True
+			# for i, c in enumerate(cnts):
+			# 	# if the contour is too small, ignore it
+			# 	if cv2.contourArea(c) < MIN_MOTION:
+			# 		continue
+				# has_motion = True
 				# # compute the bounding box for the contour, draw it on the frame,
 				# # and update the text
 				# (x, y, w, h) = cv2.boundingRect(c)
@@ -70,29 +91,48 @@ class camera:
 
 			self.lastImage = gray
 
-			if has_motion:
-				return thresh
+			# if has_motion:
+			return thresh
 
 		return None
 
-	def background_substract(self, image):
-		if self.avg1 == None:
-			self.avg1 = np.float32(image)
-			self.avg2 = np.float32(image)
-
+	def background(self, image):
+		if self.background_avg == None:
+			self.background_avg = np.float32(image)
 		else:
-			cv2.accumulateWeighted(image,avg1,0.1)
-		    cv2.accumulateWeighted(image,avg2,0.01)
+			cv2.accumulateWeighted(image,self.background_avg,BACKGROUND_acc_weight)
+			background = cv2.convertScaleAbs(self.background_avg)
+			return background
 
-		    res1 = cv2.convertScaleAbs(avg1)
-		    res2 = cv2.convertScaleAbs(avg2)
-
-		    # cv2.imshow('img',f)
-		    # cv2.imshow('avg1',res1)
-		    # cv2.imshow('avg2',res2)
-		    return rest2
 		return None
 
+	def background_subtract(self, image):
+		foremat=self.bgs.apply(image, learningRate=BACKGROUND_acc_weight)
+		# foremat=self.bgs.apply(image)
+		# ret,thresh = cv2.threshold(foremat,127,255,0)
+		# contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		# if len(contours) > 0:
+		# 	m= np.mean(contours[0],axis=0)
+		return foremat
+
+	def mincut_maxflow(self, image):
+		# Create the graph.
+		g = maxflow.Graph[int]()
+		# Add the nodes. nodeids has the identifiers of the nodes in the grid.
+		nodeids = g.add_grid_nodes(image.shape)
+		# Add non-terminal edges with the same capacity.
+		g.add_grid_edges(nodeids, 50)
+		# Add the terminal edges. The image pixels are the capacities
+		# of the edges from the source node. The inverted image pixels
+		# are the capacities of the edges to the sink node.
+		g.add_grid_tedges(nodeids, image, 255-image)
+		# Find the maximum flow.
+		g.maxflow()
+		# Get the segments of the nodes in the grid.
+		sgm = g.get_grid_segments(nodeids)
+		# The labels should be 1 where sgm is False and 0 otherwise.
+		img2 = np.logical_not(sgm) * image
+		return img2
 
 	def motion_callback(self, data):
 		try:
@@ -103,7 +143,11 @@ class camera:
 		motion_image = None
 
 		if self.method == 1:
-			self.background_substract(cv_image)
+			motion_image = self.background_subtract(cv_image)
+			# background = self.background(cv_image)
+			# motion_image = cv_image - background
+		elif self.method == 2:
+			motion_image = self.mincut_maxflow(cv_image)
 		else:
 			motion_image = self.image_diff(cv_image)
 
