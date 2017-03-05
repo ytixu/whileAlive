77b50a3b9b11a226f93ddffa37c1d3dbf845deb3
yitian <yi.t.xu@mail.mcgill.ca>
2017-03-05 11:11:26 -0500

    new model

--- robotics/project_17/src/agent/src/fcn/fcn_agent.py
+++ robotics/project_17/src/agent/src/fcn/fcn_agent.py
@@ -15,7 +15,7 @@ from keras.models import Model, load_model
 from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
 
 N_CLASSES = 2
-BATCH_SIZE = 65
+BATCH_SIZE = 100
 smooth = 1
 
 def dice_coef(y_true, y_pred):
@@ -30,66 +30,89 @@ def dice_coef_loss(y_true, y_pred):
 
 class fcn_agent:
 	def __init__(self, data_topic, load_file=None):
+		if load_file:
+			self.compiled = True
+			self.trained = True
+			self.model = load_model(load_file,
+				custom_objects={'dice_coef_loss' : dice_coef_loss,
+								'dice_coef' : dice_coef})
+		else:
+			self.compiled = False
+			self.trained = False
+
 		self.data_sub = rospy.Subscriber(data_topic,SensorImages,self.data_update)
 
 		self.bridge = CvBridge()
 		self.training_data = [None, None]
-		self.compiled = False
-		self.trained = False
 		self.training = False
 		self.sample_n = 0
 
-		if load_file:
-			self.model = load_model(load_file,
-				custom_objects={'dice_coef_loss' : dice_coef_loss,
-								'dice_coef' : dice_coef})
-			self.compiled = True
-			self.trained = True
 
 	def _build_model(self, shape):
 		inputs = Input((3, shape[0], shape[1]))
-		conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
-		conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+		conv1 = Convolution2D(128, 8, 8, activation='sigmoid', border_mode='same')(inputs)
+		conv1 = Convolution2D(128, 8, 8, activation='sigmoid', border_mode='same')(conv1)
 		pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-		conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
-		conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+		conv2 = Convolution2D(64, 16, 16, activation='sigmoid', border_mode='same')(conv1)
+		conv2 = Convolution2D(64, 16, 16, activation='sigmoid', border_mode='same')(conv2)
 		pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-		conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
-		conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+		conv3 = Convolution2D(32, 32, 32, activation='sigmoid', border_mode='same')(pool2)
+		conv3 = Convolution2D(32, 32, 32, activation='sigmoid', border_mode='same')(conv3)
 		pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-		conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
-		conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
-		pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+		conv4 = Convolution2D(16, 64, 64, activation='sigmoid', border_mode='same')(pool3)
+		conv4 = Convolution2D(16, 64, 64, activation='sigmoid', border_mode='same')(conv4)
+		# pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-		# conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(pool4)
-		# conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(conv5)
+		up5 = merge([UpSampling2D(size=(2, 2))(conv4), conv3], mode='concat', concat_axis=1)
+		up6 = merge([UpSampling2D(size=(2, 2))(up5), conv2], mode='concat', concat_axis=1)
+		conv7 = Convolution2D(1, 1, 1, activation='relu')(up9)
+
+		# conv1 = Convolution2D(32, 5, 5, activation='sigmoid', border_mode='same')(inputs)
+		# conv1 = Convolution2D(32, 5, 5, activation='sigmoid', border_mode='same')(conv1)
+		# pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+		# conv2 = Convolution2D(64, 5, 5, activation='sigmoid', border_mode='same')(pool1)
+		# conv2 = Convolution2D(64, 5, 5, activation='sigmoid', border_mode='same')(conv2)
+		# pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+		# conv3 = Convolution2D(128, 5, 5, activation='sigmoid', border_mode='same')(pool2)
+		# conv3 = Convolution2D(128, 5, 5, activation='sigmoid', border_mode='same')(conv3)
+		# pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+		# conv4 = Convolution2D(256, 5, 5, activation='sigmoid', border_mode='same')(pool3)
+		# conv4 = Convolution2D(256, 5, 5, activation='sigmoid', border_mode='same')(conv4)
+		# pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+		# conv5 = Convolution2D(512, 5, 5, activation='sigmoid', border_mode='same')(pool4)
+		# conv5 = Convolution2D(512, 5, 5, activation='sigmoid', border_mode='same')(conv5)
 
 		# up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
-		# conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(up6)
-		# conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv6)
+		# conv6 = Convolution2D(256, 5, 5, activation='sigmoid', border_mode='same')(up6)
+		# conv6 = Convolution2D(256, 5, 5, activation='sigmoid', border_mode='same')(conv6)
 
-		up7 = merge([UpSampling2D(size=(2, 2))(conv4), conv3], mode='concat', concat_axis=1)
-		conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up7)
-		conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv7)
+		# up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
+		# conv7 = Convolution2D(128, 5, 5, activation='sigmoid', border_mode='same')(up7)
+		# conv7 = Convolution2D(128, 5, 5, activation='sigmoid', border_mode='same')(conv7)
 
-		up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
-		conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
-		conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv8)
+		# up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
+		# conv8 = Convolution2D(64, 5, 5, activation='sigmoid', border_mode='same')(up8)
+		# conv8 = Convolution2D(64, 5, 5, activation='sigmoid', border_mode='same')(conv8)
 
-		up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
-		conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
-		conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
+		# up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
+		# conv9 = Convolution2D(32, 5, 5, activation='sigmoid', border_mode='same')(up9)
+		# conv9 = Convolution2D(32, 5, 5, activation='sigmoid', border_mode='same')(conv9)
 
-		conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(conv9)
+		# conv10 = Convolution2D(1, 1, 1, activation='relu')(conv9)
 
 		self.model = Model(input=inputs, output=conv10)
 
+		# self.model.compile(optimizer='sgd', loss=dice_coef_loss, metrics=[dice_coef])
 		self.model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
 
-
+		# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 		# self.model.compile(optimizer='sgd', loss='mse', metrics=['accuracy'])
 		###'MSE' -- >  'binary_crossentropy'
 
@@ -110,36 +133,41 @@ class fcn_agent:
 			print(e)
 
 		_,out_image = cv2.threshold(out_image,127,255,cv2.THRESH_BINARY)
-		# cv2.imshow('training', out_image)
-
-		# if self.training_data[1] == None:
-		# 	self.training_data[0] = np.array([cv2.split(in_image)])
-		# 	self.training_data[1] = np.array([cv2.split(out_image)])
-		# else:
-		# 	self.training_data[0] = np.append(self.training_data[0],
-		# 									  np.array([cv2.split(in_image)]),
-		# 									  axis=0)
-		# 	self.training_data[1] = np.append(self.training_data[1],
-		# 									  np.array([cv2.split(out_image)]),
-		# 									  axis=0)
-		# self.sample_n += 1
-		# if not self.trained and self.sample_n > BATCH_SIZE:
-		# 	self.update()
-
-		# if not self.compiled:
-		# 	self._build_model(in_image.shape)
-		# 	return
+		self.sample_n += 1
+		# cv2.imshow('0', in_image)
+		# cv2.imshow('1', out_image)
+		# cv2.waitKey(1)
 
 		if self.trained:
 			prediction = self.model.predict_on_batch(np.array([cv2.split(in_image)]))
 			image = np.array(prediction[0][0])
-			_,image_gray = cv2.threshold(image,0.75,1,cv2.THRESH_BINARY)
+			_,image_gray = cv2.threshold(image,0.5,1,cv2.THRESH_BINARY)
 			# image = np.zeros([image.shape[0],image.shape[1],1])
 			# image[:,:,0] = image
 			# image_gray = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 			cv2.imshow('Prediction', image_gray)
 			cv2.waitKey(1)
+			print image
 			# print dice_coef_loss(out_image, prediction)
+		else:
+			if self.training_data[1] == None:
+				self.training_data[0] = np.array([cv2.split(in_image)])
+				self.training_data[1] = np.array([cv2.split(out_image)])
+			else:
+				self.training_data[0] = np.append(self.training_data[0],
+												  np.array([cv2.split(in_image)]),
+												  axis=0)
+				self.training_data[1] = np.append(self.training_data[1],
+												  np.array([cv2.split(out_image)]),
+												  axis=0)
+			if not self.trained and self.sample_n > BATCH_SIZE:
+				self.update()
+
+			if not self.compiled:
+				self._build_model(in_image.shape)
+				return
+
+
 
 
 	def update(self):
