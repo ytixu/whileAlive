1a500fa4cfbf4e8d209f215febe1d66a9eabebcf
yitian <yi.t.xu@mail.mcgill.ca>
2017-03-09 18:51:56 -0500

    fix

--- robotics/project_17/src/agent/src/fcn/fcn_agent.py
+++ robotics/project_17/src/agent/src/fcn/fcn_agent.py
@@ -35,32 +35,33 @@ class fcn_agent:
 		self.mask = None
 		self.orient = None
 		self.prev_frame = None
+		self.shape = None
 
-		self.estimate_segments = None
+		self.estimate_segments = {}
 		self.color_map = {}
 
 		self.data_sub = rospy.Subscriber(data_topic,SensorImages,self.data_update)
 
 	def renderSegments(self, segments):
-		seg_array = np.array([])
-		final = np.zeros(self.prev_frame.shape, np.uint8)
+		seg_array = {}
+		final = np.zeros(self.shape, np.uint8)
 		for i,_ in enumerate(segments):
 			final[...] = 0
 			cv2.drawContours(final, segments, i, 1, -1)
-			seg_array = np.append(seg_array, final)
+			seg_array[i] = final
 
 		return seg_array
 
 	def matchSegments(self, new_segments):
-		new_seg = np.zeros(self.prev_frame.shape, np.uint8)
+		new_seg = np.zeros(self.shape, np.uint8)
 		labels = {}
-		new_label = self.estimate_segments.shape[0]
+		new_label = len(self.estimate_segments)
 		for i,_ in enumerate(new_segments):
 			new_seg[...] = 0
 			max_label = new_label
 			max_entropy = 0
 			cv2.drawContours(new_seg, new_segments, i, 1, -1)
-			for j, old_seg in enumerate(self.estimate_segments):
+			for j, old_seg in self.estimate_segments.iteritems():
 				h = mutual_information(old_seg, new_seg)
 				if h > max_entropy:
 					max_entropy = h
@@ -73,13 +74,13 @@ class fcn_agent:
 		return labels
 
 	def updateSegments(self, new_segments, labels):
-		seg_array = np.array([])
-		final = np.zeros(self.prev_frame.shape, np.uint8)
-		for label, segs in enumerate(labels):
+		seg_array = {}
+		final = np.zeros(self.shape, np.uint8)
+		for label, segs in labels.iteritems():
 			final[...] = 0
 			for seg_id in segs:
-				cv2.drawContours(final, segs, seg_id, 1, -1)
-			seg_array = np.append(seg_array, final)
+				cv2.drawContours(final, new_segments, seg_id, 1, -1)
+			seg_array[label] = final
 		return seg_array
 
 	def getSegments(self, motion_image):
@@ -89,20 +90,22 @@ class fcn_agent:
 
 	def assignmentColor(self, image):
 		# final = np.zeros(image.shape, np.uint8)
-		mask = np.zeros(image.shape, np.uint8)
-		for i,_ in enumerate(self.estimate_segments):
-			mask[...] = 0
-			cv2.drawContours(mask, self.estimate_segments, i, 255, -1)
-			self.color_map[i] = cv2.mean(image, mask)
+		# mask = np.zeros(image.shape, np.uint8)
+		for i, seg in self.estimate_segments.iteritems():
+			# mask[...] = 0
+			# cv2.drawContours(mask, self.estimate_segments, i, 255, -1)
+			self.color_map[i] = cv2.mean(image,  seg)
 			# cv2.drawContours(final, cnts, i, color, -1)
 
 		# return (cnts, final)
 
 	def viz(self):
-		final = np.zeros(image.shape, np.uint8)
-		mask = np.zeros(image.shape, np.uint8)
-		for i, seg in enumerate(self.segments):
-			mask[...] = self.color_map[i]
+		final = np.zeros(self.prev_frame.shape, np.uint8)
+		mask = np.zeros(self.prev_frame.shape, np.uint8)
+		for i, seg in self.estimate_segments.iteritems():
+			mask[:,:,0] = self.color_map[i][0]
+			mask[:,:,1] = self.color_map[i][1]
+			mask[:,:,2] = self.color_map[i][2]
 			final = final + cv2.bitwise_and(mask, mask, mask=seg)
 
 		return final
@@ -117,7 +120,8 @@ class fcn_agent:
 
 		segments = self.getSegments(motion_image)
 
-		if self.estimate_segments == None:
+		if self.prev_frame == None:
+			self.shape = motion_image.shape
 			self.prev_frame = seg_viz.copy()
 			self.estimate_segments = self.renderSegments(segments)
 			self.assignmentColor(in_image_raw)
