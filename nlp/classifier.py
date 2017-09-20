1cf1cc040a6b94176404bcc4c9a0c12a2fe26357
ytixu <yi.t.xu@mail.mcgill.ca>
2017-09-20 15:41:21 -0400

    Update classifier.py

--- nlp/classifier.py
+++ nlp/classifier.py
@@ -1,6 +1,7 @@
 import numpy as np
 import nltk
 from nltk.corpus import stopwords
+from nltk.classify.naivebayes import NaiveBayesClassifier
 from sklearn.model_selection import KFold
 
 FILES = 'rt-polaritydata/rt-polarity.'
@@ -11,37 +12,77 @@ def get_data(filename):
 class SentimentClassifier:
 
 	def __init__(self):
-		self.p = {}
-		self.lexicon = {}
-		self.test_data = None
+		self.classifier = None
+		self.test_pos = None
+		self.test_neg = None
 
-	def get_next_fold(self, data, k=5):
+		self.stop_words = set(stopwords.words('english'))
+		self.stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
+
+
+	def get_next_fold(self, X_pos, X_neg, k=5):
 		cross_kf = KFold(n_splits=k)
 		# get training / testing
-		for train, test in cross_kf.split(data):
-			X_train, X_test = X[train], X[test]
-			self.test_data = X_test
+		neg_sets = cross_kf.split(X_neg)
+		for i, pos_sets in enumerate(cross_kf.split(X_pos)):
+			train_pos, test_pos = pos_sets
+			train_neg, test_neg = neg_sets[i]
+
+			X_train_pos, X_test_pos = X_pos[train_pos], X_pos[test_pos]
+			X_train_neg, X_test_neg = X_neg[train_neg], X_neg[test_neg]
+
+			self.test_pos = X_test_pos
+			self.test_neg = X_test_neg
+
 			# get training / validation
-			for ttrain, valid in cross_kf.split(X_train):
-				X_ttrain, X_valid = X[ttrain], X[valid]
-				yield X_ttrain, X_valid
+			sub_neg_sets = cross_kf.split(X_train_neg)
+
+			for j, sub_pos_sets in cross_kf.split(X_train_pos):
+				ttrain_pos, val_pos = sub_pos_sets
+				ttrain_neg, val_neg = sub_neg_sets[j]
+
+				X_ttrain_pos, X_val_pos = X_pos[ttrain_pos], X_pos[val_pos]
+				X_ttrain_neg, X_val_neg = X_neg[ttrain_neg], X_neg[val_neg]
+
+				x = np.concatenate(X_ttrain_pos, X_ttrain_neg)
+				xv = np.concatenate(X_val_pos, X_val_neg)
+
+				y = np.zeros(len(x))
+				y[:len(ttrain_pos)] = 'pos'
+				y[len(ttrain_pos):] = 'neg'
+				yv = np.zeros(len(xv))
+				yv[:len(val_pos)] = 'pos'
+				yv[len(val_pos):] = 'neg'
+
+				yield x, xv, y, yv
 
 		raise StopIteration
 
-	def preprocess(self, data):
-		tokens = nltk.word_tokenize(data)
-		unigrams = [token for token in tokens if token not in stopwords.words('english')]
-		# bigrams = ngrams(unigrams, 2)
-		# yield (unigrams, bigrams)
+	def preprocess(self, data, label, n_gram=1, filter_stpw=True):
+		X = [[]]*len(data)
+		for i, entry in enumerate(data):
+			if filter_stpw:
+				tokens = nltk.word_tokenize(entry)
+				unigrams = [token for token in tokens if token not in stopwords.words('english')]
+			if n_gram == 1:
+				X[i] = (' '.join(unigrams), label[i])
+			else:
+				X[i] = ngrams(unigrams, 2)
+
+		return X
+
+	def test(data, label):
+		for t in data:
 
 	def train(self, y='pos'):
-		data = get_data(FILES+y)
+		pos_data = get_data(FILES+'pos')
+		neg_data = get_data(FILES+'neg')
 
-		for train, valid in self.get_next_fold(data):
-			for line in train:
-				for word in self.preprocess(line):
-					# add probs 
+		for x, xv, y, yv in self.get_next_fold(pos_data, neg_data):
+			# training 
+			train = self.preprocess(x, y)
+			self.classifier = NaiveBayesClassifier.train(train)
+			# validate 
 
-			for line in valid:
-				# validate 
-				
+		
+			self.test()
