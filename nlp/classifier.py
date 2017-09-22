e382ea58e36f4eef9715b1ba5aa4fdf6917f6080
ytixu <yi.t.xu@mail.mcgill.ca>
2017-09-21 21:39:41 -0400

    Update classifier.py

--- nlp/classifier.py
+++ nlp/classifier.py
@@ -1,88 +1,117 @@
+import re
+import sys
 import numpy as np
+from sklearn.cross_validation import KFold
+from sklearn import svm
 import nltk
 from nltk.corpus import stopwords
 from nltk.classify.naivebayes import NaiveBayesClassifier
-from sklearn.model_selection import KFold
+from nltk.metrics import ConfusionMatrix
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
 
 FILES = 'rt-polaritydata/rt-polarity.'
 
 def get_data(filename):
-	return open(filename, 'rU').read().split('\n'):
+	return open(filename, 'rU').read().split('\n')
 
 class SentimentClassifier:
 
 	def __init__(self):
 		self.classifier = None
-		self.test_pos = None
-		self.test_neg = None
 
 		self.stop_words = set(stopwords.words('english'))
-		self.stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
+		# self.stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
 
+		self.avg_confusion_mat = {}
+		self.accuracy = {}
 
-	def get_next_fold(self, X_pos, X_neg, k=5):
-		cross_kf = KFold(n_splits=k)
-		# get training / testing
-		neg_sets = cross_kf.split(X_neg)
-		for i, pos_sets in enumerate(cross_kf.split(X_pos)):
-			train_pos, test_pos = pos_sets
-			train_neg, test_neg = neg_sets[i]
+	def preprocess(self, data, label, n_gram=1, filter_stpw=False):
+		N = len(data)
+		X = [()]*N
 
-			X_train_pos, X_test_pos = X_pos[train_pos], X_pos[test_pos]
-			X_train_neg, X_test_neg = X_neg[train_neg], X_neg[test_neg]
-
-			self.test_pos = X_test_pos
-			self.test_neg = X_test_neg
-
-			# get training / validation
-			sub_neg_sets = cross_kf.split(X_train_neg)
-
-			for j, sub_pos_sets in cross_kf.split(X_train_pos):
-				ttrain_pos, val_pos = sub_pos_sets
-				ttrain_neg, val_neg = sub_neg_sets[j]
-
-				X_ttrain_pos, X_val_pos = X_pos[ttrain_pos], X_pos[val_pos]
-				X_ttrain_neg, X_val_neg = X_neg[ttrain_neg], X_neg[val_neg]
-
-				x = np.concatenate(X_ttrain_pos, X_ttrain_neg)
-				xv = np.concatenate(X_val_pos, X_val_neg)
-
-				y = np.zeros(len(x))
-				y[:len(ttrain_pos)] = 'pos'
-				y[len(ttrain_pos):] = 'neg'
-				yv = np.zeros(len(xv))
-				yv[:len(val_pos)] = 'pos'
-				yv[len(val_pos):] = 'neg'
-
-				yield x, xv, y, yv
-
-		raise StopIteration
-
-	def preprocess(self, data, label, n_gram=1, filter_stpw=True):
-		X = [[]]*len(data)
 		for i, entry in enumerate(data):
+		
+			sys.stdout.write("\rProcessing data [%d/%d] " % (i, N))
+			sys.stdout.flush()
+		
+			unigrams = re.sub(r'\W+ ', ' ', entry)
+			unigrams = nltk.word_tokenize(unigrams)
 			if filter_stpw:
-				tokens = nltk.word_tokenize(entry)
-				unigrams = [token for token in tokens if token not in stopwords.words('english')]
+				unigrams = [token for token in unigrams if token not in self.stop_words]
 			if n_gram == 1:
-				X[i] = (' '.join(unigrams), label[i])
+				X[i] = (dict([(ngram, True) for ngram in unigrams]), label[i])
 			else:
-				X[i] = ngrams(unigrams, 2)
+				X[i] = (dict([(ngram, True) for ngram in nltk.ngrams(unigrams, 2)]), label[i]) 
 
-		return X
 
-	def test(data, label):
-		for t in data:
+		print "Done"
+		return np.array(X)
 
-	def train(self, y='pos'):
+	def matrix_form(self, data):
+		
+
+	def update_score(self, exp_type, cm, acc):
+		if exp_type not in self.avg_confusion_mat:
+			self.avg_confusion_mat[exp_type] = np.zeros((2,2))
+			self.accuracy[exp_type] = []
+
+		self.avg_confusion_mat[exp_type][0,0] += cm['n','n']
+		self.avg_confusion_mat[exp_type][0,1] += cm['n','p']
+		self.avg_confusion_mat[exp_type][1,0] += cm['p','n']
+		self.avg_confusion_mat[exp_type][1,1] += cm['p','p']
+		self.accuracy[exp_type].append(acc)
+
+		print 'Accuracy %f' % acc
+		print(cm.pretty_format(show_percents=True, truncate=9))
+
+	def naive_bayes(self, X, Y, train, test, exp_type):
+		x, xt = X[train], X[test]
+		y, yt = Y[train], Y[test]
+
+		self.classifier = NaiveBayesClassifier.train(x)
+		acc = nltk.classify.accuracy(self.classifier, xt)
+		predictons = [self.classifier.classify(t) for t, _ in xt]
+		cm = ConfusionMatrix(yt.tolist(), predictons)
+		
+		self.update_score(exp_type, cm, acc)
+
+	def svm(self, X, Y, train, test, exp_type)
+		clf = svm.SVC()
+
+		
+
+	def train(self, k=5):
 		pos_data = get_data(FILES+'pos')
 		neg_data = get_data(FILES+'neg')
 
-		for x, xv, y, yv in self.get_next_fold(pos_data, neg_data):
-			# training 
-			train = self.preprocess(x, y)
-			self.classifier = NaiveBayesClassifier.train(train)
-			# validate 
-
+		X = np.concatenate(([(x, 'p') for x in pos_data], [(x, 'n') for x in neg_data]))
+		np.random.shuffle(X)
+		Y = np.array([l for _,l in X])
+		X = np.array([x for x,_ in X])
 		
-			self.test()
+
+		for train, test in list(KFold(len(X), n_folds=k)):
+			
+			X_ = self.preprocess(X, Y)
+			self.naive_bayes(X_, Y, train, test, 'NB-unigram')
+			X_ = self.preprocess(X, Y, 2, False)
+			self.naive_bayes(X_, Y, train, test, 'NB-bigram')
+			X_ = self.preprocess(X, Y, 1, True)
+			self.naive_bayes(X_, Y, train, test, 'NB-unigram-filtered')
+			X_ = self.preprocess(X, Y, 2, True)
+			self.naive_bayes(X_, Y, train, test, 'NB-bigram-filtered')
+
+
+		for exp_type, cm in self.avg_confusion_mat.iteritems():
+			print exp_type
+			print cm
+			print np.mean(self.accuracy[exp_type])
+
+
+
+
+# main 
+model = SentimentClassifier()
+model.train()
